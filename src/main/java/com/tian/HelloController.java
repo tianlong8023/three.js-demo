ce4a9a72e65b66cbe56040132c1081b6919a1e96
liuzhen <liuzhen@unovo.com.cn>
2018-05-28 16:57:00 +0800

    规范

---
+++ src/main/java/com/tian/HelloController.java
@@ -0,0 +1,14 @@
+package com.tian;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+@RequestMapping("/")
+public class HelloController {
+	@RequestMapping(method = RequestMethod.GET)
+	public String printWelcome() {
+		return "index";
+	}
+}
